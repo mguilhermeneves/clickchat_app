336beffeff7eeb3e20c68b1c32096ddd8369e67b
M. Guilherme Neves <mguilhermeneves@gmail.com>
2023-06-21 11:52:30 -0300

    add animation to dialog

--- lib/src/global/helpers/dialog.dart
+++ lib/src/global/helpers/dialog.dart
@@ -3,54 +3,67 @@ import 'package:flutter/material.dart';
 import '../../app_widget.dart';
 
 class Dialog {
-  Future<void> alert(
+  void alert(
     String message, {
     void Function()? onPressedOk,
-  }) async {
+  }) {
     final context = navigatorKey.currentContext!;
 
-    return showDialog(
+    showGeneralDialog(
       context: context,
-      builder: (_) => AlertDialog(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-        ),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 30,
-                horizontal: 50,
+      barrierColor: Colors.black.withOpacity(0.5),
+      transitionDuration: const Duration(milliseconds: 150),
+      barrierDismissible: true,
+      barrierLabel: '',
+      pageBuilder: (context, animation1, animation2) => Container(),
+      transitionBuilder: (context, animation1, animation2, widget) {
+        return Transform.scale(
+          scale: animation1.value,
+          child: Opacity(
+            opacity: animation1.value,
+            child: AlertDialog(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
               ),
-              child: Image.asset(
-                  'assets/images/illustrations/warning_illustration.png'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 30,
+                      horizontal: 50,
+                    ),
+                    child: Image.asset(
+                        'assets/images/illustrations/warning_illustration.png'),
+                  ),
+                  Text(message, textAlign: TextAlign.center),
+                ],
+              ),
+              actionsPadding: EdgeInsets.zero,
+              actions: [
+                Column(
+                  children: [
+                    Divider(
+                      thickness: 1,
+                      height: 1,
+                      color: Theme.of(context).colorScheme.outline,
+                    ),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          if (onPressedOk != null) onPressedOk();
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-            Text(message, textAlign: TextAlign.center),
-          ],
-        ),
-        actionsPadding: EdgeInsets.zero,
-        actions: [
-          Column(
-            children: [
-              Divider(
-                thickness: 1,
-                height: 1,
-                color: Theme.of(context).colorScheme.outline,
-              ),
-              Center(
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    if (onPressedOk != null) onPressedOk();
-                  },
-                  child: const Text('OK'),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+          ),
+        );
+      },
     );
   }
 }
